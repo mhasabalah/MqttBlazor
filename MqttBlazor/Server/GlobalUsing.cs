7408345e2e922a30354593e5a0b0db875ae3ad5e
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2023-03-19 02:32:09 +0200

    Add InfluxDBClient

---
+++ MqttBlazor/Server/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using InfluxDB.Client;
+global using InfluxDB.Client.Api.Domain;
+global using InfluxDB.Client.Writes;
+global using MQTTnet.AspNetCore;
+global using System.Text;
--- MqttBlazor/Server/Program.cs
+++ MqttBlazor/Server/Program.cs
@@ -1,6 +1,8 @@
-using MQTTnet.AspNetCore;
-using System.Text;
+const string token = "KF_fEDsran9ZEAvWF3w_I-sUkpp2jxd3VPduZhk1B0HCvJNGc3QeQ_I5vmMlEzVHN_7u1CwnPOz9F3kcGEhNuA==";
+const string bucket = "bucket";
+const string org = "Smart Farm";
 
+var _influxDbClient = new InfluxDBClient("http://localhost:8086", token);
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseKestrel(options =>
@@ -11,6 +13,9 @@ builder.WebHost.UseKestrel(options =>
 });
 
 // Add services to the container.
+
+
+
 
 builder.Services.AddMqttServer(optionsBuilder =>
 {
@@ -54,21 +59,36 @@ app.UseMqttServer(server =>
         Console.WriteLine($"Client '{e.ClientId}' connected.");
         await Task.CompletedTask;
     };
-
-    //on message
     server.InterceptingPublishAsync += async e =>
     {
+        var topic = e.ApplicationMessage.Topic;
         var payload = e.ApplicationMessage?.Payload == null ? null : Encoding.UTF8.GetString(e.ApplicationMessage?.Payload);
 
         Console.WriteLine(
             " TimeStamp: {0} -- Message: ClientId = {1}, Topic = {2}, Payload = {3}, QoS = {4}, Retain-Flag = {5}",
-
             DateTime.Now,
-            e.ApplicationMessage?.Topic,
             e.ClientId,
+            e.ApplicationMessage?.Topic,
             payload,
             e.ApplicationMessage?.QualityOfServiceLevel,
             e.ApplicationMessage?.Retain);
+
+        if (topic.StartsWith("sensors/"))
+        {
+            var sensorId = topic.Substring("sensors/".Length);
+            var value = double.Parse(payload);
+
+            var point = PointData.Measurement("sensors")
+                .Tag("sensorId", sensorId)
+                .Field("value", value)
+                .Timestamp(DateTime.UtcNow, WritePrecision.Ms);
+
+            using (var writeApi = _influxDbClient.GetWriteApi())
+            {
+                writeApi.WritePoint(point, bucket, org);
+            }
+        }
+
 
         await Task.CompletedTask;
     };
