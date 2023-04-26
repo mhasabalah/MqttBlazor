9e124947e1c686f41340267c2b1a4db8f93ee9a1
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2023-04-26 04:34:58 +0200

    Update render chart

---
+++ MqttBlazor/Client/Helpers/Sensor.cs
@@ -0,0 +1,8 @@
+﻿namespace MqttBlazor.Client;
+
+public class Sensor
+{
+    public SensorType SensorType { get; set; }
+    public decimal Value { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
+}
